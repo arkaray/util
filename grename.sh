7a24bb4237b1697e2c8b59607f76f899d862d505
Arka Ray <arkaray97@gmail.com>
2018-03-26 21:49:39 +0530

    file not found message added

--- grename.sh
+++ grename.sh
@@ -1,6 +1,8 @@
 # This script can be used to rename/move files in a git repository
 # This will also change all references to this files accordingly
 
+[ -f $1 ] || echo 'source file does not exist'
+[ -f $2 ] && echo "$2 already exists"
 echo renaming $1 to $2
 git mv $1 $2
 s1=$(echo $1 | sed -i 's/\//\\\//g')
